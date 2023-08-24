f8224c2020142550f3f99db999b162795be4e322
DianaKikacheishvili <diana.kikacheishvili@care.com>
2023-08-24 17:36:34 +0300

    UC#10 Refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(Validation.validateString("kU55\"?", 10));
+    Logger logger =  LoggerFactory.getLogger(Main.class);
+    Validation.validateString("kU55\"?", 10);
+    logger.info("run regex implementation");
   }
 }
--- src/main/java/org/example/Validation.java
+++ src/main/java/org/example/Validation.java
@@ -4,20 +4,18 @@ import java.util.regex.Pattern;
 
 public class Validation {
 
+  private Validation() {
+
+  }
+
   public static boolean validateString(String str, int length) {
     if (str.length() > length) {
       return false;
     }
-    if (!Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{\\\\|}~].*).+$")
-                .matcher(str)
-                .find()) {
-      return false;
-    }
-    if (Pattern.compile("\\s")
-               .matcher(str)
-               .find()) {
-      return false;
-    }
-    return true;
+    return Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{\\\\|}~].*).+$")
+                  .matcher(str)
+                  .find() && !Pattern.compile("\\s")
+                                     .matcher(str)
+                                     .find();
   }
 }
