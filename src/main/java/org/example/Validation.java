5601b4f2c0149c28ad3c7e27aa9ffd0642aa0a9a
DianaKikacheishvili <diana.kikacheishvili@care.com>
2023-08-24 16:42:36 +0300

    UC#10 Regex Implementation

---
+++ src/main/java/org/example/Validation.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+
+  public static boolean validateString(String str, int length) {
+    if (str.length() > length) {
+      return false;
+    }
+    if (!Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{\\\\|}~].*).+$")
+                .matcher(str)
+                .find()) {
+      return false;
+    }
+    if (Pattern.compile("\\s|\\t|\\n")
+               .matcher(str)
+               .find()) {
+      return false;
+    }
+    return true;
+  }
+}
